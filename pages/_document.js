ae35893db69b8af8bbe32673a5f364f4a5d1b835
Alexander Lloyd <a.lloyd.2@bham.ac.uk>
2017-12-20 17:11:58 +0000

    Changed CSS Framework to  bulma

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,22 @@
 import Document, { Head } from 'next/document'
 
+import Footer from '../components/Footer';
+
 export default class MyDocument extends Document {
   render () {
     const { html } = this.props;
     return (
-      <html>
+      <html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/static/favicon.ico" />
-        <meta name='viewport' content='width=device-width,minimum-scale=1' />
-        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto' />
+        <meta name="viewport" content='width=device-width,minimum-scale=1' />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css" />
       </Head>
       <body>
-      <div id='__next' dangerouslySetInnerHTML={{ __html: html }} />
+        <div id='__next' dangerouslySetInnerHTML={{ __html: html }} />
+        <Footer name="Alexander Lloyd" />
       </body>
       </html>
     )
